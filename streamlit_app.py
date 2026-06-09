10f1c760b607a3ccc6da1d7a4aca033a9c0209ad
Vamsi Mudila <vamsi.mudila@gmail.com>
2026-06-09 19:28:10 +0530

    Refactor smoothie order handling and API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,13 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
+  ingredients_string = ""
+  
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + " "
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    ingredients_string = ""
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + " "
 
     st.write(ingredients_string)
 
@@ -39,6 +41,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + ' !', icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
